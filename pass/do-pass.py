9ca2519db5a92eb6c56a1e39ffb3df33b152b601
Tsukiwy <976450759@qq.com>
2018-12-17 16:05:37 +0800

    wrote a python about pass

---
+++ pass/do-pass.py
@@ -0,0 +1,22 @@
+# -*- coding: UTF-8 -*- 
+
+# 输出 Python 的每个字母
+for letter in 'Python':
+   if letter == 'h':
+      pass
+      print ("这是 pass 块")
+   print ("当前字母 :", letter)
+
+print ("Good bye!")
+
+
+'''
+当前字母 : P
+当前字母 : y
+当前字母 : t
+这是 pass 块
+当前字母 : h
+当前字母 : o
+当前字母 : n
+Good bye!
+'''
