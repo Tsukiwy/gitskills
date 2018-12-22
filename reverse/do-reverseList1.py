7c786ead3eca158598e24a5f509b06d38ff0f311
Tsukiwy <976450759@qq.com>
2018-12-22 18:36:01 +0800

    wrote a python about reverse

---
+++ reverse/do-reverseList1.py
@@ -0,0 +1,11 @@
+# -*- coding: UTF-8 -*-
+
+def reverse(li):
+    for i in range(0, int(len(li)/2)):
+        temp = li[i]
+        li[i] = li[-i-1]
+        li[-i-1] = temp
+
+l = [1, 2, 3, 4, 5]
+reverse(l)
+print(l)
